373e67e9cb1ce7e8d755fa5ee21f360b9574beba
mertsigirci11 <62725758+mertsigirci11@users.noreply.github.com>
2022-12-24 20:12:35 +0300

    Add files via upload

---
+++ Backend/API/AutoMapperProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using DTO;
+using DTO.ContentModels;
+using DTO.Friend;
+using Models;
+using Models.ContentModels;
+using Models.Friend;
+
+
+namespace API
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            //Base
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+
+            //Auth
+            CreateMap<UserAuthDto, User>();
+            CreateMap<User, UserAuthDto>();
+
+            //ContentModels
+            CreateMap<Content, ContentDto>();
+            CreateMap<ContentDto, Content>();
+            CreateMap<ContentNotification, ContentNotificationDto>();
+            CreateMap<ContentNotificationDto, ContentNotification>();
+            CreateMap<ContentStatistics,ContentStatisticsDto>();
+            CreateMap<ContentStatisticsDto, ContentStatistics>();
+
+            //Friend
+            CreateMap<Friendship, FriendshipDto>();
+            CreateMap<FriendshipDto, Friendship>();
+            CreateMap<FriendshipStatus, FriendshipStatusDto>();
+            CreateMap<FriendshipStatusDto, FriendshipStatus>();
+        }
+    }
+}
